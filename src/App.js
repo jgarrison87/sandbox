b3b940c5d979d8d83a2eb2be645cd2c8aec4fa30
josiah <jgarrison12@hotmail.com>
2022-06-13 19:06:38 -0600

    sandbox update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Sandbox</h1>
+      <h2>Testing ground for coding projects</h2>
+    </div>
+  );
+}
+
+export default App;
